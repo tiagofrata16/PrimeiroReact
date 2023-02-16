b9dbc7d636348b5cff2febf15b7c42ba4bafe165
Tiago Frata <tiagofrata9@gmail.com>
2023-02-16 10:27:25 -0300

    Criacao de conteudo realacionado a placa de som para treino do uso de componentes React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Frase from './components/Frase1'
 import './App.css';
 
 function App() {
+  const url = "https://s2.glbimg.com/_pteQAhIBz6Sq3IQcmc5jxJficI=/0x0:695x643/640x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/r/H/SXAEmQSG2GbN8WT6Bp7Q/2016-03-21-991304-placa-de-som-asus-xonar-essence-st-3.jpg"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Placa de Som</h1>
+        <img alt="Placa de Video" src={url}/>
+        <Frase/>
     </div>
   );
 }
