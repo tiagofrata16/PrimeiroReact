d0cbeab2c5ba22c9404dfda232cec56eb3440453
Tiago Frata <tiagofrata9@gmail.com>
2023-02-16 10:38:04 -0300

    Adicao da pasta components

---
+++ src/components/Frase2.js
@@ -0,0 +1,13 @@
+import Frase3 from './Frase3';
+
+function Frase2() {
+  return (
+    <div>
+        <h2>Este recurso costuma vir no computador <br/> 
+            imbutido na placa-mãe.</h2>
+        <Frase3/>
+    </div>
+  );
+}
+
+export default Frase2;
