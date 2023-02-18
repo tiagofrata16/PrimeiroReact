d2e93fbb0734e26aad6a4b67d215e0f5516b57b6
Tiago Frata <tiagofrata9@gmail.com>
2023-02-18 11:14:19 -0300

    Adicao do conteudo HTML do projeto Landing Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import Frase from './components/Frase1'
+import Principal from './components/Principal'
+import Cabecalho from './components/Cabecalho'
 import './App.css';
 
 function App() {
-  const url = "https://s2.glbimg.com/_pteQAhIBz6Sq3IQcmc5jxJficI=/0x0:695x643/640x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/r/H/SXAEmQSG2GbN8WT6Bp7Q/2016-03-21-991304-placa-de-som-asus-xonar-essence-st-3.jpg"
   return (
     <div className="App">
-        <h1>Placa de Som</h1>
-        <img alt="Placa de Video" src={url}/>
-        <Frase/>
+        <Cabecalho/>
+        <Principal/>
     </div>
   );
 }
