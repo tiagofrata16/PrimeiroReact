4b3c86e1575ab7710327de99e1038c8cf308e5c7
Tiago Frata <tiagofrata9@gmail.com>
2023-02-19 09:40:25 -0300

    Termino da adicao do conteudo HTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Principal from './components/Principal'
 import Cabecalho from './components/Cabecalho'
+import Topicos from './components/Topicos';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
         <Cabecalho/>
         <Principal/>
+        <Topicos/>
     </div>
   );
 }
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,21 +1,10 @@
 import ImagemWeb from './img/image-web-3-desktop.jpg'
-import ImagemGaming from './img/image-gaming-growth.jpg'
-import RetroPCs from './img/image-retro-pcs.jpg'
-import TopLaptop from './img/image-top-laptops.jpg'
+import BarraLateral from './BarraLateral'
 
 function Principal() {
   return (
+    <section>
         <main>
-             <aside>
-                <h1>New</h1>
-                <h2>Hydrogen VS Electric Cars</h2>
-                <h3>Will hydrogen-fueled cars ever catch up to EVs?</h3>
-                <h2>The Downsides of Al Artistry</h2>
-                <h3>What are the possible adverse effects of on-demand AI image generation?</h3>
-                <h2>Is VC Funding Drying Up?</h2>
-                <h3>Private funding by VC firms is down 50% YOY. We take a look at what that means</h3>
-            </aside>
-        <section>
             <img alt="Imagem Web" src={ImagemWeb}/>
             <div>
                 <h1>The Bright Future of Web 3.0?</h1>
@@ -28,31 +17,9 @@ function Principal() {
                     <button>READ MORE</button>
                 </div>
             </div>
-        </section>
-        <section>
-                <img alt="RetroPCs" src={RetroPCs}/>
-                <div>
-                    <h1>Reviving Retro PCs</h1>
-                    <h2>What happens when old PCs <br/>
-                    are given modern upgrades?
-                    </h2>
-                </div>
-                <img alt="TopLaptop" src={TopLaptop}/>
-                <div>
-                    <h1>Top 10 Laptops of 2022</h1>
-                    <h2>Our best picks for various <br/>
-                    needs and budgets.
-                    </h2>
-                </div>
-                <img alt="Gaming" src={ImagemGaming}/>
-                <div>
-                    <h1>The Growth of Gaming</h1>
-                    <h2>How the pandemic has sparked <br/>
-                    fresh opportunities.
-                    </h2>
-                </div>
-        </section>
         </main>
+       <BarraLateral/>
+    </section>    
   );
 }
 
